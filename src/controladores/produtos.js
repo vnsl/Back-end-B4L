44ef3062a60aa3995a1211df8707d08c8404058a
Luiz Felipe <luizfelipe90@hotmail.com>
2021-07-28 16:59:12 -0300

    atualização rotas e criado base do controlador de produtos

---
+++ src/controladores/produtos.js
@@ -0,0 +1,41 @@
+const knex = require('../conexao');
+
+
+const listarProdutos = async (req, res) => {
+
+};
+
+const obterProduto = async (req, res) => {
+
+};
+
+const cadastrarProduto = async (req, res) => {
+
+};
+
+const atualizarProduto = async (req, res) => {
+
+};
+
+const excluirProduto = async (req, res) => {
+
+};
+
+const ativarProduto = async (req, res) => {
+
+};
+
+const desativarProduto = async (req, res) => {
+
+};
+
+
+module.exports = {
+    cadastrarProduto,
+    listarProdutos,
+    atualizarProduto,
+    obterProduto,
+    excluirProduto,
+    ativarProduto,
+    desativarProduto
+};
--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const login = require('./controladores/login');
 const usuarios = require('./controladores/usuarios');
+const produtos = require('./controladores/produtos');
 
 const rotas = express();
 
@@ -9,5 +10,15 @@ rotas.post('/login', login.login);
 
 //usuarios
 rotas.post('/usuarios', usuarios.cadastrarUsuario);
+
+//Crud de produtos
+rotas.get('/produtos', produtos.listarProdutos);
+rotas.get('/produtos/:id', produtos.obterProduto);
+rotas.post('/produtos', produtos.cadastrarProduto);
+rotas.put('/produtos/:id', produtos.atualizarProduto);
+rotas.delete('/produtos/:id', produtos.excluirProduto);
+rotas.post('/produtos/:id/ativar', produtos.ativarProduto);
+rotas.post('/produtos/:id/desativar', produtos.desativarProduto);
+
 
 module.exports = rotas;
