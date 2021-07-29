0add8a065cbf90c802abfab0edbe98315bd27758
Vinicius <viniciusnattan14@gmail.com>
2021-07-29 13:45:04 -0300

    configuração com DB da heroku - user update

--- src/conexao.js
+++ src/conexao.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         host: 'ec2-3-213-146-52.compute-1.amazonaws.com',
-        user: 'postgres',
+        user: 'sbzoezbbrusnez',
         database: 'd2fl0k5n919onm',
         password: '67b38a67b8cf647c08e17378378a770c2d40ea73c389603058c1ccbf41d2b4d1',
     }
