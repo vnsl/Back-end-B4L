503bd1551139fbd27a4951d11688e1046d9afbdd
Luiz Felipe <luizfelipe90@hotmail.com>
2021-07-29 16:50:40 -0300

    Criado filtro de autenticação

---
+++ src/filtros/autenticacao.js
@@ -0,0 +1,35 @@
+const knex = require('../conexao');
+const jwt = require('jsonwebtoken');
+const senhaHash = require('../senhaHash');
+
+const autenticacao = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    const token = authorization.replace('Bearer', '').trim();
+
+    if (!token) {
+        return res.status(404).json('Token não foi informado');
+    }
+
+    try {
+        
+        const { id } = jwt.verify(token, senhaHash);
+
+        const usuario = await knex('usuario').where({ id }).first();
+
+        if (!usuario) {
+            return res.status(404).json("Usuário não encontrado");
+        }
+
+        const { senha, ...dadosUsuario } = usuario;
+
+        req.usuario = dadosUsuario;
+
+        next();
+
+        } catch (error) {
+        return res.status(400).json(error.message);
+    }
+};
+
+module.exports = autenticacao;
