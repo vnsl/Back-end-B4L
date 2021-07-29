81e9dcff50f4eefa586b9020a321c59590b0bb68
Vinicius <viniciusnattan14@gmail.com>
2021-07-29 13:54:26 -0300

    configuração com DB da heroku - ssl

--- src/conexao.js
+++ src/conexao.js
@@ -6,6 +6,7 @@ const knex = require('knex')({
         user: 'sbzoezbbrusnez',
         database: 'd2fl0k5n919onm',
         password: '67b38a67b8cf647c08e17378378a770c2d40ea73c389603058c1ccbf41d2b4d1',
+        ssl: 'true',
     }
 });
 
