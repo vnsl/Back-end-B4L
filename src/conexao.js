7ecbb88bbefc388bcd77f709c9e8a17c0b7e6090
Vinicius <viniciusnattan14@gmail.com>
2021-07-29 13:03:04 -0300

    configuração com DB da heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,23 @@
+// Configurações Heroku
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
+        host: 'ec2-3-213-146-52.compute-1.amazonaws.com',
         user: 'postgres',
-        database: 'cubos_food',
-        password: '123456',
+        database: 'd2fl0k5n919onm',
+        password: '67b38a67b8cf647c08e17378378a770c2d40ea73c389603058c1ccbf41d2b4d1',
     }
 });
+
+// Configurações DB local
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         database: 'cubos_food',
+//         password: '123456',
+//     }
+// });
 
 module.exports = knex;
