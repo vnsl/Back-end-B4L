1860c10bb7a98053d221c81516e8efa57b6ab60b
Luiz Felipe <luizfelipe90@hotmail.com>
2021-07-29 16:49:37 -0300

    Feito cadastro de produtos

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,8 +1,17 @@
 const knex = require('../conexao');
-
+const schemaCadastroProduto = require('../validacoes/schemaCadastroProduto');
 
 const listarProdutos = async (req, res) => {
+    const { usuario } = req;
 
+    try {
+        const listaDeProdutos = await knex('produto').where('restaurante_id', '=', usuario.id);
+
+        return res.status(200).json(listaDeProdutos);
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 };
 
 const obterProduto = async (req, res) => {
@@ -10,6 +19,30 @@ const obterProduto = async (req, res) => {
 };
 
 const cadastrarProduto = async (req, res) => {
+    const { nome, descricao, preco, permiteObervacoes } = req.body;
+    const { usuario } = req;
+
+    try {
+        await schemaCadastroProduto.validate(req.body);
+
+        const mesmoRestaurante = await knex.select('id').from('restaurante').where('id', '=', usuario.id);
+
+        const restauranteId = Number(mesmoRestaurante[0].id);
+      
+        const produtoExistente = await knex('produto').where({ nome }).first();
+
+        if (produtoExistente) {
+            return res.status(400).json("O produto já cadastrado");
+        }
+
+        const produto = await knex('produto').insert({
+            'nome': nome, descricao, 'preco': preco, 'permite_observacoes': permiteObervacoes, 'restaurante_id': restauranteId }).returning('*');
+        
+        return res.status(200).json('Produto cadastrado com sucesso');
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 
 };
 
