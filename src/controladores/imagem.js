087fbfe48c274c534cfed4a8ac1470bf0bb470ec
jeanrvos <velasco.jean@gmail.com>
2021-08-08 20:02:58 -0300

    atualiza nome do arquivo

--- src/controladores/imagem.js
+++ src/controladores/imagem.js
@@ -1,7 +1,9 @@
 const supabase = require('../supabase');
 
 const enviarImagem = async(req, res) => {
-    const { nome, imagem } = req.body;
+    const { imagem } = req.body;
+
+    const randomName = Date.now();
 
     const imagemConvertida = imagem.replace("data:image/jpeg;base64,", "");
     // const nomeImagem = nome.replace(" ", "");
@@ -16,7 +18,7 @@ const enviarImagem = async(req, res) => {
         const { data, error } = await supabase
             .storage
             .from(process.env.SUPABASE_BUCKET)
-            .upload(`${nome}`, buffer);
+            .upload(`${randomName}`, buffer);
 
         if(error){
             return res.status(400).json(error.message);
@@ -25,7 +27,7 @@ const enviarImagem = async(req, res) => {
         const { publicURL, error: errorPublicUrl } = supabase
             .storage
             .from(process.env.SUPABASE_BUCKET)
-            .getPublicUrl(nome);
+            .getPublicUrl(randomName);
 
             if(errorPublicUrl){
                 return res.status(400).json(errorPublicUrl.message);
