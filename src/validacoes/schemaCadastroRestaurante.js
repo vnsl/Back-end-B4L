79b08b526981b73c94e93958b35b71e69ced5642
jeanrvos <velasco.jean@gmail.com>
2021-08-08 11:13:53 -0300

    atualiza nome dos campos do body no yup

--- src/validacoes/schemaCadastroRestaurante.js
+++ src/validacoes/schemaCadastroRestaurante.js
@@ -2,10 +2,10 @@ const yup = require('./configuracoes');
 
 const schemaCadastroRestaurante = yup.object().shape({
         nome: yup.string().required(),
-        idCategoria: yup.number().required(),
-        taxaEntrega: yup.number().required(),
-        tempoEntregaEmMinutos: yup.number().required(),
-        valorMinimoPedido: yup.number().required()
+        categoria_id: yup.number().required(),
+        taxa_entrega: yup.number().required(),
+        tempo_entrega_minutos: yup.number().required(),
+        valor_minimo_pedido: yup.number().required()
 });
 
 
