eba2c3c690df15407232a8e30e09425ab1845898
jeanrvos <velasco.jean@gmail.com>
2021-08-27 16:05:24 -0300

    ajuste tabela consumidor

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -5,12 +5,30 @@ const listarPedidos = async (req, res) => {
     const { restaurante } = usuario;
 
     try {
-        const detalhesPedido = await knex('pedido').leftJoin('detalhepedido', 'pedido.id', 'detalhepedido.pedido_id')
-        .leftJoin('consumidor', 'pedido.consumidor_id', 'consumidor.id').leftJoin('endereco', 'consumidor.id', 'endereco.consumidor_id')
-        .where('pedido.restaurante_id', '=', restaurante.id);    
+        const detalhesPedido = await knex('pedido')
+        .where('pedido.restaurante_id', '=', restaurante.id)
+        .leftJoin('detalhepedido', 'pedido.id', 'detalhepedido.pedido_id')
+        .leftJoin('consumidor', 'pedido.consumidor_id', 'consumidor.id')
+        .leftJoin('endereco', 'consumidor.id', 'endereco.consumidor_id')
+        .leftJoin('produto', 'produto.id', 'detalhepedido.produto_id');
 
+        const arrayPedidos = await detalhesPedido.map(pedido => {
+            const pedidoIndividual = {
+              id: pedido.pedido_id,
+              nome_produto: pedido.nome,
+              imagem_produto: pedido.imagem,
+              qtd_produto: pedido.quantidade_produto,
+              endereco: pedido.endereco,
+              cep: pedido.cep,
+              complemento: pedido.complemento,
+              nome_cliente: pedido.nome_consumidor,
+              valor_total: pedido.valor_total_produto
+            //   somar valor total com a taxa de entrega no front
+            }
+            return pedidoIndividual;
+        });
 
-        return res.status(200).json(detalhesPedido);
+        return res.status(200).json(arrayPedidos);
         
     } catch (error) {
         return res.status(400).json(error.message);
